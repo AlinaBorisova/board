af407eb3e3473c02425fb25c86fe804f9f08b5ee
Алина Борисова <b-alina1999.borisova@yandex.ru>
2025-08-14 12:11:50 +0300

    Подготовка страницы с карточками

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -8,7 +8,7 @@ export function App() {
     location.pathname === ROUTES.LOGIN || location.pathname === ROUTES.REGISTER;
 
   return (
-    <div>
+    <div className="bg-amber-200">
       {!isAuthPage && <AppHeader />}
       <Outlet />
     </div>
---
+++ src/shared/api/schema/index.ts
@@ -0,0 +1,4 @@
+import type { paths, components } from "@/shared/api/schema/generated.ts";
+
+export type ApiPaths = paths;
+export type ApiSchemas = components["schemas"];
